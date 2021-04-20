8f71b0221066d6aeb8a0c232828c0233a09a893c
Kathryn Kane <Kathryn@Kathryns-MacBook-Pro-2.local>
2021-04-19 19:05:17 -0700

    deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,4 @@ summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearanc
 # Deliverable 2
 Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
 total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
